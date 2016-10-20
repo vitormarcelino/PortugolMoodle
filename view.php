8535e1d766b40b555b9b4edde08959fcd730cde2
vitormarcelino <vitormarcelino93@gmail.com>
2016-10-20 14:09:02 -0300

    Altera Estrutura

--- view.php
+++ view.php
@@ -77,7 +77,9 @@ if ($portugol->intro) {
 }
 
 // Replace the following lines with you own code.
-echo $OUTPUT->heading('Yay! It works!');
+echo $OUTPUT->heading('Portugol Moodle');
+
+require 'ambiente.html';
 
 // Finish the page.
 echo $OUTPUT->footer();
