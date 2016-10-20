a8721877dc8a08113675df90b66ee9d85726f156
vitormarcelino <vitormarcelino93@gmail.com>
2016-10-20 13:47:12 -0300

    Adiciona o s arquivos do plugin

---
+++ lang/en/portugol.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for portugol
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_portugol
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'portugol';
+$string['modulenameplural'] = 'portugols';
+$string['modulename_help'] = 'Use the portugol module for... | The portugol module allows...';
+$string['portugol:addinstance'] = 'Add a new portugol';
+$string['portugol:submit'] = 'Submit portugol';
+$string['portugol:view'] = 'View portugol';
+$string['portugolfieldset'] = 'Custom example fieldset';
+$string['portugolname'] = 'portugol name';
+$string['portugolname_help'] = 'This is the content of the help tooltip associated with the portugolname field. Markdown syntax is supported.';
+$string['portugol'] = 'portugol';
+$string['pluginadministration'] = 'portugol administration';
+$string['pluginname'] = 'portugol';
