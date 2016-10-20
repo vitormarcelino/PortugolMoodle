c2ca3a123f6f006407233f29597a9cbbcd052c68
vitormarcelino <vitormarcelino93@gmail.com>
2016-10-20 13:57:06 -0300

    Altera o version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_portugol';
-$plugin->version = 0;
+$plugin->version = 1;
 $plugin->release = 'v0.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
